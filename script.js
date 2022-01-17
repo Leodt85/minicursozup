14b6517c60e2e82ef21fa9923483d934ee55a26b
Leodt85 <leodt85@gmail.com>
2022-01-16 23:41:18 -0300

    Ajustes no JS

--- script.js
+++ script.js
@@ -4,6 +4,37 @@ const nomeDoPersonagem = document.querySelector('#nome');
 const especie = document.querySelector('#especie');
 const condicao = document.querySelector('#status');
 
+
+gerarValorAletorio = () => {
+	return Math.floor(Math.random() *671);
+};
+
+chamarApi = (numeroAleatorio) => {
+	return fetch(`https://rickandmortyapi.com/api/character/${numeroAleatorio}`, {
+        method:'GET',
+        headers: { 
+            Accept: 'application/json',
+            "Content-type": 'application/json'
+        }
+    });
+}
+atualizarTela = () => {
+    pegarPersonagemChamarApiAlterarDadosImagem(1);
+    pegarPersonagemChamarApiAlterarDadosImagem(2);
+    pegarPersonagemChamarApiAlterarDadosImagem(3);
+}
+
+pegarPersonagemChamarApiAlterarDadosImagem = (idImagem) => {
+    let numeroAleatorio = gerarValorAletorio();
+
+    chamarApi(numeroAleatorio)
+        .then(
+            (respostaDaApi) => respostaDaApi.json())
+            .then((data) => {
+                alterarDadosImagem(data,idImagem);
+        });
+}
+
 traduzirCondicao = (data) => {
     if(data.status == 'unknown'){
         return 'Não sabemos';
@@ -13,35 +44,19 @@ traduzirCondicao = (data) => {
         return 'Não. Está morto';
     }
 }
+alterarDadosImagem = (data, idImagem) => {
+    const imagem = document.querySelector('#img' + idImagem);
+    const detalhesContainer = document.querySelector('#detalhes-container' + idImagem);
+    const nomeDoPersonagem = detalhesContainer.querySelector('#nome');
+    const especie = detalhesContainer.querySelector('#especie');
+    const condicao = detalhesContainer.querySelector('#status');   
+    imagem.src = data.image;
+    imagem.alt = data.name; 
+    imagem.title = data.name;   
+    nomeDoPersonagem.innerHTML = data.name;
+    especie.innerHTML = data.species;
+    condicao.innerHTML = traduzirCondicao(data);
 
-gerarValorAletorio = () => 
-{	return Math.floor(Math.random() *671);}; 
-chamarApi = (numeroAleatorio) => {	return 
-fetch(`https://rickandmortyapi.com/api/character/${numeroAleatorio}`, { 
-method:'GET', headers: { Accept: 'application/json', "Content-type":
-'application/json' } });}atualizarTela = () => { 
-    pegarPersonagemChamarApiAlterarDadosImagem(1); 
-    pegarPersonagemChamarApiAlterarDadosImagem(2); 
-    pegarPersonagemChamarApiAlterarDadosImagem(3);} 
-    pegarPersonagemChamarApiAlterarDadosImagem = (idImagem) => { let 
-        numeroAleatorio = gerarValorAletorio(); chamarApi(numeroAleatorio) .then( 
-            (respostaDaApi) => respostaDaApi.json()) .then((data) => { 
-                alterarDadosImagem(data,idImagem); });} traduzirCondicao = (data) => 
-                { if(data.status == 'unknown'){ 
-                    return 'Não sabemos'; }
-                    else if(data.status == 'Alive'){ 
-                    return 'Sim'; }else {
-                    return 'Não. Está morto'; }}alterarDadosImagem = (data, idImagem) => { 
-                    const imagem = document.querySelector('#img' + idImagem); const detalhesContainer = 
-                    document.querySelector('#detalhes-container' + idImagem); const nomeDoPersonagem =
-                    detalhesContainer.querySelector('#nome'); const especie = 
-                    detalhesContainer.querySelector('#especie'); const condicao = 
-                    detalhesContainer.querySelector('#status'); 
-                    imagem.src = data.image; 
-                    imagem.alt = data.name; 
-                    imagem.title = data.name; 
-                    nomeDoPersonagem.innerHTML = data.name; 
-                    especie.innerHTML = data.species; condicao.innerHTML = traduzirCondicao(data); 
 }
-botao.onclick = VerConhecerPersonagem;
 
+botao.onclick = VerConhecerPersonagem;
